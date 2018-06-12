26dfb0f884f7b1471e67b09f6af1f3ada44513cf
alhe123 <37627657+alhe123@users.noreply.github.com>
2018-06-12 15:34:36 -0500

    Delete todo-spec.js

--- ProtractorTests/todo-spec.js
+++
@@ -1,8 +0,0 @@
-describe('CDBU Modern Software Factory UI Test - nateDevollModernSof-env-preprod Environment', function() {
-  it('should add a todo', function() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://msf-preprod.cdbu.io');
-    element(by.css('[name="name"]')).sendKeys('Selenium Test User\ue007');
-    //element(by.buttonText('submit')).click();
-    });
-});
